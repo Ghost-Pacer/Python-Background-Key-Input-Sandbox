c73f6fd5784b8f240cbe9c513878adbdd2da11fd
Ronith Kandallu <ronithkandallu@gmail.com>
2020-10-24 12:56:48 -0700

    Attempt to run background thread with asyncio.run_coroutine_threadsafe

--- main.py
+++ main.py
@@ -1,19 +1,36 @@
 import time
+import asyncio
+import threading
 import keyboard
 
+pressed_a: bool = False
+pressed_b: bool = False
 
-def main():
+
+async def main():
+    keypress_loop = asyncio.new_event_loop()
+    threading.Thread(target=keypress_loop.run_forever).start()
+    future = asyncio.run_coroutine_threadsafe(listen_for_keypress(), keypress_loop)
+
     while True:
-        time.sleep(2)
+        time.sleep(0.5)
         print("main is executing...")
-        listen_for_keypress()
+        print("Pressed a: " + bool_to_string(pressed_a))
+        print("Pressed b: " + bool_to_string(pressed_b))
 
 
-def listen_for_keypress():
-    if keyboard.is_pressed("a"):
-        print("test")
+async def listen_for_keypress():
+    while True:
+        if keyboard.is_pressed("a"):
+            pressed_a = True
+        elif keyboard.is_pressed("b"):
+            pressed_b = True
+
+
+def bool_to_string(bool_to_convert: bool) -> str:
+    return "True" if bool_to_convert else "False"
 
 
 if __name__ == '__main__':
     print("hello")
-    main()
+    asyncio.run(main())
